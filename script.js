e58ca74ee1f3f003c840a48bbaecafbacd66fe99
Rob <robjanssens93@hotmail.com>
2021-03-01 13:14:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hallo Winc Academy studenten")
+
+//dit is een grote som//
+
+/*let a = 1230941;
+let b = 1823794;
+c = a * b;
+
+console.log(c);
+
+let d = 637263;
+let e = 54;
+f = d / e;
+console.log(f);*/
+
+
+
+
